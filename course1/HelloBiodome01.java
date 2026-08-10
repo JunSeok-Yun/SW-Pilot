64ee820bec573433abf052ae70d3b4bc5d00aff7
윤준석 <ujs9901@naver.com>
2026-08-10 17:18:05 +0900

    refactor : 예외처리 조건 추가

--- course1/HelloBiodome01.java
+++ course1/HelloBiodome01.java
@@ -1,6 +1,6 @@
 public class HelloBiodome01 {
 	public static void main(String[] args) {
-		if(args.length == 0 ) {
+		if(args.length == 0 || args[0].trim().length() == 0) {
 			System.out.println("입력된 값이 없습니다. 다시 입력해주세요.");
 			return;
 		}
